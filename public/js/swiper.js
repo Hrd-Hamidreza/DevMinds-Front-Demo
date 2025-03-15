aa5a212a181d13cbd2ce423f5bd36091debec0fc
Hrd-Hamidreza <hamidrezadaraee.hd@gmail.com>
2025-03-15 12:59:35 +0330

    Responsive Home

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,4 +1,23 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = (swiper = new Swiper(".mySwiper", {
+  speed: 800,
+  spaceBetween: 0,
   watchSlidesProgress: true,
-  slidesPerView: 3,
-});
+  breakpoints: {
+    // when window width is >= 1200px
+    0: {
+      slidesPerView: 1.01,
+    },
+    // when window width is >= 1200px
+    768: {
+      slidesPerView: 2.01,
+    },
+    // when window width is >= 1200px
+    992: {
+      slidesPerView: 3.01,
+    },
+    // when window width is >= 1200px
+    1800: {
+      slidesPerView: 4.01,
+    },
+  },
+}));
